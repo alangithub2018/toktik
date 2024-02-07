5a1b915833fb3a1d8bde250174314c683b1236c7
alangithub2018 <43253925+alangithub2018@users.noreply.github.com>
2024-02-07 00:29:06 -0600

    Initial commit

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,52 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'https://dr0wg8d51az6r.cloudfront.net/1_k3u3h7iz.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'https://vod-progressive.akamaized.net/exp=1707290134~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F210%2F17%2F426050453%2F1846336854.mp4~hmac=1416f06f24b9d7dfd599cb3eea101517f676d9278b48e7f2ddeaa4a80b6c42ea/vimeo-prod-skyfire-std-us/01/210/17/426050453/1846336854.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': 'https://vod-progressive.akamaized.net/exp=1707290583~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3011%2F17%2F440059748%2F1923130279.mp4~hmac=ac2cb0024e8fafa452adc9112054b23c284ba5eaac9b5cbd19ed85a4c0290d8b/vimeo-prod-skyfire-std-us/01/3011/17/440059748/1923130279.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': 'https://dr0wg8d51az6r.cloudfront.net/1_k3u3h7iz.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'El COVID no me afecta',
+    'videoUrl': 'https://dr0wg8d51az6r.cloudfront.net/1_k3u3h7iz.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'No quiero ir a trabajar hoy señor Stark',
+    'videoUrl': 'https://dr0wg8d51az6r.cloudfront.net/1_k3u3h7iz.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Limpiar nunca fue tan divertido',
+    'videoUrl': 'https://dr0wg8d51az6r.cloudfront.net/1_k3u3h7iz.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': 'https://dr0wg8d51az6r.cloudfront.net/1_k3u3h7iz.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+
+
+];
